ef86dae0962e7c02530349165d85d3c3e1235ee5
demon3030313 <167988848+demon3030313@users.noreply.github.com>
2024-05-30 14:15:07 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 // Вместо нашего адреса вставь свой
 // Можно ещё не удалять адреса с localhost, но мы удалили для краткости
-const allowedCors = ["https://hikkback.nomorepartiesco.ru/", "localhost"];
+const allowedCors = ["https://hikkfront.nomorepartiesco.ru/", "localhost"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
